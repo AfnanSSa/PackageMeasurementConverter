219a8124ab2143bb3897f84de137ffe0589688ba
AfnanSSa <iafnanz67@gmail.com>
2024-03-20 12:06:29 +0400

    initial commit: Base

---
+++ Task.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class PackageConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nEnter a package: ");
+        String input = scanner.nextLine();
+        
+	//call method to encode
+    }
+
+//method to encode input
+}
